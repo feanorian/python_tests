09b6d9c6fb20ce087b094258aa18f3b306059642
Craig L Brooks <Craig.Brooks95@myhunter.cuny.edu>
2015-05-28 13:02:23 -0400

    These are my first python modules

---
+++ new_module.py
@@ -0,0 +1,9 @@
+''' test importing stuff from a module'''
+
+val1 = 100.
+
+list2 = range(10)
+
+def print_me(something):
+    '''prints something'''
+    print something
---
+++ tuple_functions.py
@@ -0,0 +1,11 @@
+''' Several useful functions for doing things with tuples
+ '''
+#order does not matter with keyword arguments
+
+def tuple_value_change_kw(a_tuple=(2,3,4,5),an_int=6):
+    '''this function changes the 1st value of a tuple'''
+    tupnew = (an_int,)
+    '''newtuple[0:] gets the first value of the tuple'''
+    tupnew = tupnew + a_tuple[0:]
+    print tupnew + a_tuple[0:]
+    return tupnew
